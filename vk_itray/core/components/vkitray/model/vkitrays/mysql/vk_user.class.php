94f3e01a87da335c0a047ba8d6e2f5b9ae73a62e
wowkos <wowkos@mail.ru>
2017-10-22 10:38:14 +0300

    Removed extra folders

--- vk_itray/core/components/vkitray/model/vkitrays/mysql/vk_user.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/vk_user.class.php');
-class vk_user_mysql extends vk_user {}
--- vk_itray/core/components/vkitray/model/vkitrays/vk_user.class.php
+++
@@ -1,4 +0,0 @@
-<?php
-class vk_user extends xPDOSimpleObject {
-    
-}
